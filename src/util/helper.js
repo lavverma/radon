847ee1361a15f9c57264b0aaf94f972b0018fd09
unknown <lavverma109@gmail.com>
2022-06-02 10:53:12 +0530

    helper file

---
+++ src/util/helper.js
@@ -0,0 +1,10 @@
+const date=function printDate(){
+    return "01/06/2022";
+}
+const month=function printMonth(){
+    return "June";
+}
+const info=function getBatchInfo(){
+    return "Radon,W3D3, the topic for today is Nodejs module system";
+}
+module.exports={date,month,info};
