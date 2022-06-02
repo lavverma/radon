19d88ba9b3a0969d79c3d8990da1e13f472b5007
unknown <lavverma109@gmail.com>
2022-06-02 10:54:28 +0530

    format file

---
+++ src/validator/formatter.js
@@ -0,0 +1,13 @@
+const trim=function trim(){
+    let a="  lav verma  " ;
+    return a.trim();
+}
+const lower=function changeToLowerCase(){
+    let a="LAV VERMA" ;
+    return a.toLowerCase();
+}
+const upper=function changeToUpperCase(){
+    let a="functionup" ;
+    return a.toUpperCase();
+}
+module.exports={trim,lower,upper};
