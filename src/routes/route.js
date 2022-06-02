233b9da3d940ee4f12e95aaeea41e05cbca5482b
unknown <lavverma109@gmail.com>
2022-06-02 23:50:53 +0530

    with lodash

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,30 +1,43 @@
 const express = require('express');
-const externalModule = require('./logger')
+const _=require('lodash');
+const externalModule = require('../logger/logger');
+const {date,month,info}=require('../util/helper');
+const {trim,lower,upper}=require('../validator/formatter');
 
 const router = express.Router();
 
 router.get('/test-me', function (req, res) {
-    console.log('The constant in logger route has a value '+externalModule.endpoint)
-    console.log('The current batch is '+externalModule.batch)
-    externalModule.log()
+    console.log(externalModule.welcome());
+    console.log(date());
+    console.log(month());
+    console.log(info());
+    console.log(trim());
+    console.log(lower());
+    console.log(upper());
     res.send('My first ever api!')
 });
 
-router.get('/test-me1', function (req, res) {
-    res.send('My second ever api!')
+router.get('/hello',function(req,res){
+    const monthNames = ["January", "February", "March", "April", "May", "June",
+"July", "August", "September", "October", "November", "December"
+];
+console.log(_.chunk(monthNames,4));
+
+const oddNumbers=[1,3,5,7,9,11,13,15,17,19];
+console.log(_.tail(oddNumbers,9));
+
+const arr1=[11,26,33,4];
+const arr2=[31,26,3,54];
+const arr3=[1,20,3,40];
+const arr4=[11,20,1,4];
+const arr5=[1,31,4,8];
+console.log(_.union(arr1,arr2,arr3,arr4,arr5));
+
+console.log(_.fromPairs([["horror","The Shining"],["drama","Titanic"],["thriller","Shutter Island"],["fantasy","Pans Labyrinth"]]))
+res.send('this is testing apis')
 });
 
-router.get('/test-me2', function (req, res) {
-    res.send('My third api!')
-});
 
-router.get('/test-me3', function (req, res) {
-    res.send('My 4th api!')
-});
-
-router.get('/test-me4', function (req, res) {
-    res.send('My last api!')
-});
 
 module.exports = router;
 // adding this comment for no reason
