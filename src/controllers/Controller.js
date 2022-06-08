293f4b6768df064528fee472217159a88a341bca
lavverma <lavverma109@gmail.com>
2022-06-08 18:43:16 +0530

    withOptionalProblem

--- src/controllers/Controller.js
+++ src/controllers/Controller.js
@@ -37,8 +37,9 @@ const updatedPriceBookAuthor=async function(req,res){
         {$set: {price: 100}},
         {new :true}
     )
-    let updatedPriceAuthor=await authorModel.find({author_id:updatePrice.author_id}).select({author_name:1})
-    res.send({msg: updatedPriceAuthor})
+    let updatedPrice=updatePrice.price
+    let updatedPriceAuthor=await authorModel.find({author_id:updatePrice.author_id}).select({author_name:1,_id:0})
+    res.send({updatedPriceAuthor,updatedPrice})
 
 }
 module.exports.updatedPriceBookAuthor=updatedPriceBookAuthor
@@ -53,3 +54,32 @@ const authorName=async function(req,res){
     res.send({msg:author})
 }
 module.exports.authorName=authorName
+
+
+const bookByAuthorId1=async function(req,res){
+   let book=await (await bookModel.find({author_id:1}).select({name:1, _id:0})).map(obj=>obj.name)
+   res.send(book)
+} 
+module.exports.bookByAuthorId1=bookByAuthorId1
+
+const bookByAuthorId2=async function(req,res){
+    let book=await (await bookModel.find({author_id:2}).select({name:1, _id:0})).map(obj=>obj.name)
+    res.send(book)
+ } 
+ module.exports.bookByAuthorId2=bookByAuthorId2
+
+ const bookByAuthorId3=async function(req,res){
+    let book=await (await bookModel.find({author_id:3}).select({name:1, _id:0})).map(obj=>obj.name)
+    let books=[...new Set(book)]
+    res.send(books)
+ } 
+ module.exports.bookByAuthorId3=bookByAuthorId3
+
+
+const authorAndAge=async function(req,res){
+    let books=await (await bookModel.find({rating:{$gt: 4}})).map(obj=>obj.author_id)
+    let id=[...new Set(books)]
+    let authorAge=await authorModel.find({$and:[{author_id:id},{age:{$gt:50}}]}).select({author_name:1,age:1,_id:0})
+     res.send({msg:authorAge})
+}
+module.exports.authorAndAge=authorAndAge
--- src/routes/route.js
+++ src/routes/route.js
@@ -2,15 +2,21 @@ const express = require('express');
 const router = express.Router();
 
 const BookController= require("../controllers/Controller")
+
 router.post("/createAuthor", BookController.createAuthor)
-
-
-
 router.post("/createBook", BookController.createBook  )
+
+
+
 router.get("/getBooksData", BookController.getAuthorBookData)
 router.get("/updatedPriceBookAuthor", BookController.updatedPriceBookAuthor)
 router.get("/authorName", BookController.authorName)
 
+router.get("/bookByAuthorId/1", BookController.bookByAuthorId1)
+router.get("/bookByAuthorId/2", BookController.bookByAuthorId2)
+router.get("/bookByAuthorId/3", BookController.bookByAuthorId3)
+
+router.get("/authorAndAge", BookController.authorAndAge)
 
 
 
